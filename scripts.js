e362ca9e6f96f81a0cf2d088ad9e210aeb7ed8c3
Aleksandra Bury <aleksandra.w.bury@gmail.com>
2021-06-10 08:31:51 +0200

    school exercises 2.0

--- scripts.js
+++ scripts.js
@@ -14,3 +14,19 @@ document.addEventListener('DOMContentLoaded', function()
         alert("hello again, " + name)
     });
 });
+
+//* ternary operator chained - looking for matching pairs within 3 variables
+
+let answer = 
+    (mangoes == oranges) && (apples == mangoes) ? "They are all equal"
+    : (mangoes == oranges)  ? "Only mangoes are equal to oranges"
+    : (apples == mangoes) ? "Only apples are equal to mangoes"
+    : (apples == oranges)  ? "Only apples are equal to oranges": "No pair is equal"
+console.log(`Check for equal numbers among the three given variables:\n${answer}.`);
+
+//* removing empty spaces
+
+for (let i = 0; string.includes('  '); i++) {
+string = string.replace('  ',' ').trim();
+}
+console.log(string);
