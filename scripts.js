b7a05de919e8f9063abb765dc415e6757ff32ffc
Aleksandra Wiktoria Bury <aleksandra.w.bury@gmail.com>
2021-06-13 15:29:12 +0200

    homework boolean added

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
-function greet()
+/*function greet()
 {
     let name = document.querySelector("#name").value;
     alert('hello, ' + name);
 }
-
-
 
 document.addEventListener('DOMContentLoaded', function()
 { 
@@ -13,4 +11,22 @@ document.addEventListener('DOMContentLoaded', function()
         let name = document.querySelector('#name2').value;
         alert("hello again, " + name)
     });
-});
+}); */
+
+/*document.addEventListener('DOMContentLoaded', function()
+{ 
+    document.querySelector('#form3').addEventListener('submit', function patADog ()
+    {
+        let dogCheck = (document.querySelector('#dogCheck').value).toLowerCase();
+            
+        dogCheck.includes('woof') ? alert('pat, pat') : alert('find me a dog to pat!'); 
+    }
+    )});
+*/
+
+    function patADog ()
+    {
+        let dogCheck = (document.querySelector('#dogCheck').value).toLowerCase();
+            
+        dogCheck.includes('woof') ? alert('pat, pat') : alert('Find me a dog to pat!'); 
+    }
